cb9fc162dca6a939d7886b465202732fc09d35c1
przybylo.dawid@gmail.com <przybylo.dawid@gmail.com>
2018-12-04 12:17:12 +0100

    init commit

---
+++ src/Klaska.java
@@ -0,0 +1,10 @@
+public class Klaska {
+    public static void main(String[] args) {
+
+        metodka();
+
+    }
+    public static void metodka(){
+        System.out.println("Hello world");
+    }
+}
